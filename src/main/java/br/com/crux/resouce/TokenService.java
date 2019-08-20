01ee97f265331a8bd88474a32d497179413a85b8
Wilson Souza <wilsons80@gmail.com>
2019-08-20 09:02:13 -0300

    Ajustes no serviço de refresh-token

--- src/main/java/br/com/crux/resouce/TokenService.java
+++ src/main/java/br/com/crux/resouce/TokenService.java
@@ -2,17 +2,16 @@ package br.com.crux.resouce;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.crux.cmd.AutenticadorCmd;
 import br.com.crux.to.UsuarioLogadoTO;
 
-
+@RestController
 @RequestMapping(value = "token")
-@Component
 public class TokenService {
 	
 	@Autowired
