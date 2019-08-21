ec4942c285033b3f5ea303bfde90cf945d95743d
Wilson Souza <wilsons80@gmail.com>
2019-08-20 22:32:56 -0300

    Inicio da implementaçao da Dao para buscar as querys no banco de dados

---
+++ src/main/java/br/com/crux/dao/base/GetQueryDao.java
@@ -0,0 +1,13 @@
+package br.com.crux.dao.base;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GetQueryDao {
+
+	
+	public String getQuery() {
+		
+		return null;
+	}
+}
