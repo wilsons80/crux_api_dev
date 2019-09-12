df831261177ec0fb270bc48b07941e14f591676c
josue.rodrigues <josue.rodrigues@mpdft.mp.br>
2019-09-12 15:03:10 -0300

    tirando id

--- src/main/java/br/com/crux/service/UsuarioService.java
+++ src/main/java/br/com/crux/service/UsuarioService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class UsuarioService {
 	@GetMapping(path = "/unidade", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<UsuarioUnidadeTO> getUsuariosPorUnidadeLogada() {
 		return getUsuariosPorUnidadeCmd.getUsuariosPorUnidade();
+	}
+	
+	@GetMapping(path = "/unidade/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<UsuarioUnidadeTO> getUsuariosPorUnidade(@PathVariable(name = "id") Long idUnidade) {
+		return getUsuariosPorUnidadeCmd.getUsuariosPorUnidade(idUnidade);
 	}
 	
 }
