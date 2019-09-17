fc2cafeb5dfba650d8ea9e715f7c7c9058417e8a
Wilson Souza <wilsons80@gmail.com>
2019-09-17 13:37:13 -0300

    Ajuste no Filter para carregar a unidade logada

--- src/main/java/br/com/crux/security/UnidadeLogadaFilter.java
+++ src/main/java/br/com/crux/security/UnidadeLogadaFilter.java
@@ -1,6 +1,8 @@
 package br.com.crux.security;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -12,24 +14,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.crux.cmd.CarregarUnidadeLogadaCmd;
 import br.com.crux.cmd.GetUsuarioLogadoCmd;
 import br.com.crux.cmd.SaveUsuarioLogadoCmd;
+import br.com.crux.service.exception.ApiError;
 import br.com.crux.to.AcessoUnidadeTO;
 import br.com.crux.to.UsuarioLogadoTO;
 
 @Component
-public class CookieUnidadeLogadaFilter implements Filter {
+public class UnidadeLogadaFilter implements Filter {
 	
 
 	@Autowired private CarregarUnidadeLogadaCmd carregarUnidadeLogadaCmd;
 	@Autowired private GetUsuarioLogadoCmd getUsuarioLogadoCmd;
 	@Autowired private SaveUsuarioLogadoCmd saveUsuarioLogadoCmd;
 	
-	public CookieUnidadeLogadaFilter() {
+	public UnidadeLogadaFilter() {
     }
 
 	public void destroy() {
@@ -39,15 +46,32 @@ public class CookieUnidadeLogadaFilter implements Filter {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
 		
-		if(req.getRequestURI().contains("/unidade/logada/")) {
-			String unidadeLogada = req.getRequestURI().replaceAll("/unidade/logada/", "");
+		try {
 			
-			Authentication auth = getUsuarioLogadoCmd.get();
-			saveUsuarioLogadoCmd.save(auth);
+			if(req.getRequestURI().contains("/unidade/logada/")) {
+				String unidadeLogada = req.getRequestURI().replaceAll("/unidade/logada/", "");
+				
+				Authentication auth = getUsuarioLogadoCmd.get();
+				saveUsuarioLogadoCmd.save(auth);
+				
+				AcessoUnidadeTO unidadeLogadaTO = carregarUnidadeLogadaCmd.carregarUnidadeLogada(Long.valueOf(unidadeLogada));
+				UsuarioLogadoTO usuarioLogado = getUsuarioLogadoCmd.getUsuarioLogado();
+				usuarioLogado.setUnidadeLogada(unidadeLogadaTO);
+			}
 			
-			AcessoUnidadeTO unidadeLogadaTO = carregarUnidadeLogadaCmd.carregarUnidadeLogada(Long.valueOf(unidadeLogada));
-			UsuarioLogadoTO usuarioLogado = getUsuarioLogadoCmd.getUsuarioLogado();
-			usuarioLogado.setUnidadeLogada(unidadeLogadaTO);
+		} catch (Exception e) {
+			ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), new Date());
+			PrintWriter writer = res.getWriter();
+			
+			ObjectMapper mapper = new ObjectMapper();
+			String apiErrorString = mapper.writeValueAsString(apiError);
+			
+			writer.write(apiErrorString);
+			
+			res.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+			res.setStatus(HttpStatus.BAD_REQUEST.value());
+			
+			return;
 		}
 		
 		
