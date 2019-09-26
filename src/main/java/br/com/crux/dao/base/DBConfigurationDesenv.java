e4d28dfe70d7e19e0b68ef4907104772784b5489
Wilson Souza <wilsons80@gmail.com>
2019-09-26 14:19:12 -0300

    Ajustes na configuração para acessar o banco postgres do heroku no localhost

--- src/main/java/br/com/crux/dao/base/DBConfigurationDesenv.java
+++ src/main/java/br/com/crux/dao/base/DBConfigurationDesenv.java
@@ -4,26 +4,29 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 
-@Profile("prod")
+@Profile("dev")
 @Configuration
-public class DataConfiguration {
+public class DBConfigurationDesenv {
 
+	@Value("${heroku.database.url}")
+	private String dbUrl;
 	
 	@Bean
     public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        URI dbUri = new URI(dbUrl);
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath(); // + "?sslmode=require";
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
 
         System.out.println("=====================================================================================================================================");
-        System.out.println("Criando conexção na URL: " + dbUrl);
+        System.out.println("DESENVOLVIMENTO: Criando conexção na URL: " + dbUrl);
         System.out.println("=====================================================================================================================================");
         
         
---
+++ src/main/java/br/com/crux/dao/base/DBConfigurationProd.java
@@ -0,0 +1,38 @@
+package br.com.crux.dao.base;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+
+@Profile("prod")
+@Configuration
+public class DBConfigurationProd {
+
+	
+	@Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath(); // + "?sslmode=require";
+
+        System.out.println("=====================================================================================================================================");
+        System.out.println("PRODUÇÃO: Criando conexção na URL: " + dbUrl);
+        System.out.println("=====================================================================================================================================");
+        
+        
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+	
+}
