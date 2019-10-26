621139a25af71b0c85f39e634e3facba59ffaf87
Wilson Souza <wilsons80@gmail.com>
2019-10-26 13:37:38 -0200

    Ajuste no serviço de reporovação aluno

--- src/main/java/br/com/crux/builder/ReprovacoesAlunoTOBuilder.java
+++ src/main/java/br/com/crux/builder/ReprovacoesAlunoTOBuilder.java
@@ -1,14 +1,11 @@
 package br.com.crux.builder;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import br.com.crux.cmd.GetAlunoCmd;
-import br.com.crux.entity.Aluno;
 import br.com.crux.entity.ReprovacoesAluno;
 import br.com.crux.to.ReprovacoesAlunoTO;
 
@@ -16,7 +13,6 @@ import br.com.crux.to.ReprovacoesAlunoTO;
 public class ReprovacoesAlunoTOBuilder {
 
 	@Autowired private AlunoTOBuilder alunoBuilder;
-	@Autowired private GetAlunoCmd getAlunoCmd;
 
 	public ReprovacoesAluno build(ReprovacoesAlunoTO p) {
 		ReprovacoesAluno retorno = new ReprovacoesAluno();
@@ -26,12 +22,6 @@ public class ReprovacoesAlunoTOBuilder {
 		retorno.setDescricaoMotivo(p.getDescricaoMotivo());
 		retorno.setSerieReprovacao(p.getSerieReprovacao());
 		retorno.setQtdReprovacao(p.getQtdReprovacao());
-		/*
-		Optional.ofNullable(p.getAluno()).ifPresent(a -> {
-			Aluno aluno = getAlunoCmd.getById(a.getId());
-			retorno.setAluno(aluno);
-		});
-		*/
 		retorno.setAluno(alunoBuilder.build(p.getAluno()));
 		
 		retorno.setUsuarioAlteracao(p.getUsuarioAlteracao());
