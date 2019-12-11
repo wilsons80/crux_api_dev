bb6f37031442ab4eabce374408bb61cc83736ad3
josue.rodrigues <josue.rodrigues@mpdft.mp.br>
2019-12-11 17:50:26 -0300

    - Ajuste materiais programa

--- src/main/java/br/com/crux/cmd/GetMateriaisProgramaCmd.java
+++ src/main/java/br/com/crux/cmd/GetMateriaisProgramaCmd.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Component;
 
 import br.com.crux.builder.MateriaisProgramaTOBuilder;
 import br.com.crux.dao.repository.MateriaisProgramaRepository;
+import br.com.crux.dao.repository.ProgramaRepository;
 import br.com.crux.entity.MateriaisPrograma;
 import br.com.crux.entity.Programa;
+import br.com.crux.exception.base.NegocioException;
 import br.com.crux.to.MateriaisProgramaTO;
 
 @Component
@@ -18,15 +20,22 @@ public class GetMateriaisProgramaCmd {
 
 	@Autowired private MateriaisProgramaRepository repository;
 	@Autowired private MateriaisProgramaTOBuilder toBuilder;
+	@Autowired private ProgramaRepository programaRepository;
 
-	public List<MateriaisProgramaTO> getComposicaoRhProgramaByPrograma(Programa param) {
-		Optional<List<MateriaisPrograma>> lista = repository.findByPrograma(param);
+	public List<MateriaisProgramaTO> getMateriaisProgramaByPrograma(Programa programa) {
+		Optional<List<MateriaisPrograma>> lista = repository.findByPrograma(programa);
 
 		if (lista.isPresent()) {
 			return toBuilder.buildAll(lista.get());
 		}
 
 		return Collections.emptyList();
+
+	}
+
+	public List<MateriaisProgramaTO> getMateriaisProgramaByIdPrograma(Long idPrograma) {
+		Programa programa = programaRepository.findById(idPrograma).orElseThrow(() -> new NegocioException("Programa não encontrado"));
+		return getMateriaisProgramaByPrograma(programa);
 
 	}
 
