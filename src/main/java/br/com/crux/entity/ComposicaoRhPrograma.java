228609d8635079508f7f69c0da368fb64c59f2fe
josue.rodrigues <josue.rodrigues@mpdft.mp.br>
2019-12-11 15:43:38 -0300

    - long

--- src/main/java/br/com/crux/entity/ComposicaoRhPrograma.java
+++ src/main/java/br/com/crux/entity/ComposicaoRhPrograma.java
@@ -22,7 +22,7 @@ public class ComposicaoRhPrograma  {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sq_id_composicao")
 	@SequenceGenerator(name = "sq_id_composicao", sequenceName = "sq_id_composicao", schema = Constantes.SCHEMA_PUBLIC, initialValue = 1, allocationSize = 1)
 	@Column(name="id_composicao")
-	private long id;
+	private Long id;
 
 	@ManyToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="id_cargo")
@@ -45,11 +45,11 @@ public class ComposicaoRhPrograma  {
 	public ComposicaoRhPrograma() {
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
