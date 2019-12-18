c886b4f9349c3933729b0bd4cdaeadb7b4afe98e
Wilson Souza <wilsons80@gmail.com>
2019-12-18 15:10:56 -0200

    Implementação da tela de Oficinas

--- src/main/java/br/com/crux/service/AtividadeService.java
+++ src/main/java/br/com/crux/service/AtividadeService.java
@@ -2,13 +2,22 @@ package br.com.crux.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.crux.cmd.AlterarAtividadeCmd;
+import br.com.crux.cmd.CadastrarAtividadesCmd;
+import br.com.crux.cmd.ExcluirAtividadeCmd;
 import br.com.crux.cmd.GetAtividadeCmd;
 import br.com.crux.to.AtividadesTO;
 
@@ -17,6 +26,9 @@ import br.com.crux.to.AtividadesTO;
 public class AtividadeService {
 
 	@Autowired private GetAtividadeCmd getCmd;
+	@Autowired private ExcluirAtividadeCmd excluirCmd;
+	@Autowired private AlterarAtividadeCmd alterarCmd;
+	@Autowired private CadastrarAtividadesCmd cadastrarCmd;
 
 	@GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<AtividadesTO> getAll() {
@@ -38,5 +50,21 @@ public class AtividadeService {
 		return getCmd.getTOById(id);
 	}
 
+	@PostMapping(path = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+	@Transactional
+	public void cadastrar(@RequestBody AtividadesTO param) {
+		cadastrarCmd.cadastrar(param);
+	}
+
+	@PutMapping(path = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+	@Transactional
+	public void alterar(@RequestBody AtividadesTO param) {
+		alterarCmd.alterar(param);
+	}
+
+	@DeleteMapping(path = "/{id}")
+	public void excluir(@PathVariable(name = "id") Long id) {
+		excluirCmd.excluir(id);
+	}
 
 }
