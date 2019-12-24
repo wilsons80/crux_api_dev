0779c05a80a11be6b204f8ca29e0c2f1863253bf
Wilson Souza <wilsons80@gmail.com>
2019-12-24 19:18:05 -0200

    Valida o cadastro de pessoa fisica com o mesmo cpf

--- src/main/java/br/com/crux/cmd/AlterarPessoaFisicaCmd.java
+++ src/main/java/br/com/crux/cmd/AlterarPessoaFisicaCmd.java
@@ -1,11 +1,14 @@
 package br.com.crux.cmd;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.crux.builder.PessoaFisicaTOBuilder;
 import br.com.crux.dao.repository.PessoaFisicaRepository;
 import br.com.crux.entity.PessoaFisica;
+import br.com.crux.exception.PessoaFisicaJaExisteException;
 import br.com.crux.rule.CamposObrigatoriosPessoaFisicaRule;
 import br.com.crux.to.PessoaFisicaTO;
 
@@ -19,6 +22,11 @@ public class AlterarPessoaFisicaCmd {
 	public PessoaFisica alterar(PessoaFisicaTO to) {
 		
 		camposObrigatoriosPessoaFisicaRule.verificar(to);
+		
+		Optional<PessoaFisica> pessoa = repository.findByCpf(to.getCpf());
+		if(pessoa.isPresent() && !pessoa.get().getId().equals(to.getId())) {
+			throw new PessoaFisicaJaExisteException("Já existe um registro cadastrado com esse CPF: " + to.getCpf());
+		}
 		
 		PessoaFisica pessoaFisica = pessoaFisicaTOBuilder.build(to);
 		
